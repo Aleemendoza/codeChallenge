bd644109552c015c1be37f34dc0dbd38bae20e20
Aleemendoza <alejandromendoza.dev@gmail.com>
2020-08-25 23:52:38 -0300

    funcionalidad basica de auth en progreso

---
+++ api/src/services/auth.js
@@ -0,0 +1,52 @@
+// requerimientos de modulo 
+//modulo de autenticacion, maneja la autenticacion de la api de hiringRoom
+//expone un metoodo que devuelve un token vigente
+//para que el token este vigente debe conocer cuando vence y renovarlo antes que expire 
+const axios = require('axios')
+
+let token = "";
+
+function obtenerToken(){
+
+	return token;
+
+}
+
+function actualizarToken(){
+
+	setTimeout(() => {
+
+		const data = {
+			client_id : "misentrevistas",
+			client_secret: "c6a36be30918192c645b5bb87f185054"
+		};
+
+		const options = {
+
+			headers: {
+
+				'Content-Type': 'application/json',
+			}
+		};
+
+		axios.post('https://api.hiringroom.com/v0/authenticate/login', data, options)
+		.then(res =>{
+			token = res.data.token
+		})
+
+	}, 2000);
+
+};
+
+
+function setearToken(tokenSeteado){
+
+	token = tokenSeteado;
+
+}
+
+module.exports = {
+
+	obtenerToken
+
+}
